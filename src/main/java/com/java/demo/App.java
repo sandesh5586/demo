0bfae54403c89a0d7a869345110fc8aece08796e
sandesh <sandesh.bly19@gmail.com>
2017-08-13 15:10:08 +0530

    feature

--- src/main/java/com/java/demo/App.java
+++ src/main/java/com/java/demo/App.java
@@ -1,6 +1,6 @@
 package com.java.demo;
 
-/**
+/*
  * Hello world!
  *
  */
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World feature !" );
     }
 }
